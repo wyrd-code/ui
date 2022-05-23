b67fa683acca29ca89d6cb8c785aabf38b31d0b2
Tomislav Glavaš <tomo.glavas@gmail.com>
2022-05-24 00:58:00 +0200

    dx: add standard-release package

---
+++ .versionrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+  types: [
+    {
+      type: "feat",
+      section: "Features"
+    },
+    {
+      type: "fix",
+      section: "Bug Fixes"
+    },
+    {
+      type: "chore",
+      hidden: true
+    },
+    {
+      type: "dx",
+      hidden: true
+    },
+    {
+      type: "docs",
+      hidden: true
+    },
+    {
+      type: "style",
+      hidden: true
+    },
+    {
+      type: "refactor",
+      hidden: true
+    },
+    {
+      type: "perf",
+      hidden: true
+    },
+    {
+      type: "test",
+      hidden: true
+    },
+  ],
+}
