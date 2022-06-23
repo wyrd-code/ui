a1e88f8231a2018a9a70bb83481e5d97defa844a
Tomislav Glavaš <tomo.glavas@gmail.com>
2022-06-23 17:25:15 +0200

    fix: appending home route to auto breadcrumbs

--- src/components/breadcrumbs/WuiBreadcrumbsAuto.vue
+++ src/components/breadcrumbs/WuiBreadcrumbsAuto.vue
@@ -5,22 +5,37 @@
 </template>
 
 <script setup lang="ts">
+import { defineProps, computed, PropType } from 'vue'
 import { useRoute } from 'vue-router'
 
 import type { WyrdNavItem } from '../../ui.types'
 import WuiNav from '../nav/WuiNav.vue'
 import WuiBreadcrumbs from './WuiBreadcrumbs.vue'
 
+const props = defineProps({
+  homeRoute: {
+    type: Object as PropType<WyrdNavItem>,
+    default: null,
+  },
+})
+
 const route = useRoute()
 const matched = route.matched.filter((r) => r.name)
 
-const items: WyrdNavItem[] = matched.map((r) => {
-  const label = (r.meta?.title as string) || (r.name as string)
-  const to = r.name ? { name: r.name } : undefined
+const matchedItems = computed<WyrdNavItem[]>(() =>
+  matched.map((r) => {
+    const label = (r.meta?.title as string) || (r.name as string)
+    const to = r.name ? { name: r.name } : undefined
 
-  return {
-    label,
-    to,
-  }
-})
+    return {
+      label,
+      to,
+    }
+  })
+)
+const items = computed<WyrdNavItem[]>(() =>
+  props.homeRoute
+    ? [props.homeRoute, ...matchedItems.value]
+    : matchedItems.value
+)
 </script>
