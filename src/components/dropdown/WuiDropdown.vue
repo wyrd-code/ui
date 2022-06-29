ca69f71e3856aa6723bf0d5b2a504b40f1beeffa
Tomislav Glavaš <tomo.glavas@gmail.com>
2022-06-30 01:27:30 +0200

    fix: WuiDropdown dark mode

--- src/components/dropdown/WuiDropdown.vue
+++ src/components/dropdown/WuiDropdown.vue
@@ -23,8 +23,44 @@
   @apply border-b;
   @apply last:(border-none rounded-b-md) first:rounded-t-md hover:(underline);
   @apply bg-neutral-50 border-neutral-200 text-neutral-700;
-  @apply dark:(bg-neutral-900 border-neutral-800 text-neutral-300);
+  @apply dark:(bg-neutral-800 border-neutral-700 text-neutral-300);
 }
 .wui-dropdown-item-active {
 }
+.v-popper--theme-dropdown .v-popper__wrapper .v-popper__inner {
+  @apply shadow-lg;
+  @apply bg-neutral-100 border-neutral-200;
+  @apply dark:(bg-neutral-800 border-neutral-700);
+  @apply dark:(shadow-black);
+}
+.v-popper--theme-dropdown .v-popper__arrow-container {
+}
+.v-popper--theme-dropdown .v-popper__arrow-container .v-popper__arrow-inner {
+  @apply border-neutral-100;
+  @apply dark:border-neutral-800;
+}
+.v-popper--theme-dropdown .v-popper__arrow-container .v-popper__arrow-outer {
+  @apply border-neutral-200;
+  @apply dark:border-neutral-700;
+}
+
+/* Transition */
+
+/*
+.v-popper--theme-dropdown.v-popper__popper--hidden {
+  visibility: hidden;
+  opacity: 0;
+  transition: opacity 0.5s, visibility 0.5s;
+}
+
+.v-popper--theme-dropdown.v-popper__popper--shown {
+  visibility: visible;
+  opacity: 1;
+  transition: opacity 0.5s;
+}
+
+.v-popper--theme-dropdown.v-popper__popper--skip-transition {
+  transition: none !important;
+}
+*/
 </style>
